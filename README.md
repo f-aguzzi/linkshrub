17b473887da83cff7c3f3f5338240512bc321a34
Federico Aguzzi <federico.aguzzi@icloud.com>
2023-03-15 16:06:56 +0100

    First working demo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # linkshrub
 linktr.ee-styled landing page generator
+
+# Instructions
+
+## Starting the demo
+
+### Install
+From the main folder:
+
+'yarn install'
+
+### Startup
+
+'yarn start'
+
+### Editing the links
+
+While making sure to maintain the formatting provided in the example,
+
+- edit the name and description in 'src/data.json'
+- edit the links in 'src/links.json'
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const linkTest = () => {
+import links from "./links.json"
+import data from "./data.json"
+
+const linkButton = (name: string, desc: string, link: string) => {
   return (
-    <button className="bg-red-500">
-      <h1 className="text-2xl">link</h1>
-    </button>
+    <a href={link} className="bg-purple-400 rounded-full p-2 m-2 shadow-2xl hover:bg-purple-300 hover:animate-pulse">
+      <h1 className="text-3xl p-1 text-white text-center">{name}</h1>
+      <p className="text-xl p-1 text-slate-100 text-center">{desc}</p>
+    </a>
   )
 }
 
+const linksArray = () => {
+  return links.map(elem => {
+    return linkButton(elem.name, elem.desc, elem.link)
+  })
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-6xl"> LinkShrub </h1>
-      <div className="grid grid-cols-1 place-content-center">
-        {linkTest()}
-        {linkTest()}
-        {linkTest()}
+    <div className="bg-gradient-to-b from-blue-500 to-pink-500 min-h-screen w-screen">
+      <div className="flex flex-col mx-8">
+        <h1 className="text-6xl font-bold drop-shadow-md text-slate-100 p-5 mx-auto"> {data.name} </h1>
+        <p className="text-xl font-bold drop-shadow-sm text-slate-200 p-3 mx-auto">
+          {data.desc}
+        </p>
+      </div>
+      <div className="flex flex-col mx-8">
+        {linksArray()}
       </div>
     </div>
   );
