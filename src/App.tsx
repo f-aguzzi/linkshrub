ddc86e7ad717568b3acc940f21d36c1026aa0c30
Federico Aguzzi <federico.aguzzi@icloud.com>
2023-03-15 15:26:21 +0100

    First prototype mockup

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const linkTest = () => {
+  return (
+    <button className="bg-red-500">
+      <h1 className="text-2xl">link</h1>
+    </button>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-6xl"> LinkShrub </h1>
+      <div className="grid grid-cols-1 place-content-center">
+        {linkTest()}
+        {linkTest()}
+        {linkTest()}
+      </div>
     </div>
   );
 }
